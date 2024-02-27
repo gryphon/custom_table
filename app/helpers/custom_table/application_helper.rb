11fd592d76d454f95b50897a4e65a540af297033
Grigory <mail@grigor.io>
2024-02-27 23:50:36 +0300

    changes

--- app/helpers/custom_table/application_helper.rb
+++ app/helpers/custom_table/application_helper.rb
@@ -92,7 +92,7 @@ module CustomTable
         elsif item.class.columns_hash[field.to_s] && item.class.columns_hash[field.to_s].type == :boolean
           return boolean_icon(item.send(field)) rescue ""
         elsif item.class.defined_enums.has_key?(field.to_s)
-          return (item.send(field).presence || not_set).to_s rescue ""
+          return (item.send("human_#{field}") rescue (item.send(field).presence || not_set)).to_s rescue ""
         elsif item.class.columns_hash[field.to_s] && [:date, :datetime].include?(item.class.columns_hash[field.to_s].type)
           return (item.send(field).blank? ? not_set : l(item.send(field), format: :short)) rescue ""
         elsif item.class.columns_hash[field.to_s] && [:integer, :float, :decimal].include?(item.class.columns_hash[field.to_s].type)
