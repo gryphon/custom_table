a4cee330a3e1c9be810482a3f990f184be5f3a52
Grigory <mail@grigor.io>
2026-01-10 00:09:52 +0300

    Better compartability for dummy app

--- spec/dummy/db/migrate/20230518163616_devise_create_users_dummy.rb
+++ spec/dummy/db/migrate/20230518163616_devise_create_users_dummy.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[7.0]
+class DeviseCreateUsersDummy < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
